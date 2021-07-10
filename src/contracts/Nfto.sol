131496df7bedfd41172a2ceaed937c5a794e3415
smaster0517 <smaster0517@gmail.com>
2021-07-10 16:15:24 +0530

    Truffle compiled

--- src/contracts/Nfto.sol
+++ src/contracts/Nfto.sol
@@ -4,9 +4,9 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Ocean is ERC721{
+contract Nfto is ERC721{
     
-    constructor() public ERC721("Ocean", "OCE") {}
+    constructor() public ERC721("Nfto", "NFTO") {}
     
     uint tokenID;
     
