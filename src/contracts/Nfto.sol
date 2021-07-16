305bcffc9f01a6fe31529e4b791999526c4c4883
smaster0517 <smaster0517@gmail.com>
2021-07-16 19:00:23 +0530

    Added offers in contract

--- src/contracts/Nfto.sol
+++ src/contracts/Nfto.sol
@@ -9,21 +9,31 @@ contract Nfto is ERC721{
     constructor() public ERC721("Nfto", "NFTO") {}
     
     uint tokenID;
+    uint offerCount;
     uint eCount;
     uint dCount;
     
     mapping(uint => Item) items;
+    mapping(uint => Offer) offers;
     mapping(uint => Item) english;
     mapping(uint => Item) dutch;
 
     struct Item {
         uint id;
-        uint256 price;
+        uint256 latestPrice;
         string name;
         string description;
         string cid;
         bool isListed;
         address payable owner;
+    }
+
+    struct Offer {
+        uint id;
+        uint tId;
+        uint256 offerAmount;
+        address bidder;
+        bool isApproved;
     }
 
     event ItemMinted(
@@ -32,6 +42,14 @@ contract Nfto is ERC721{
         string description,
         address payable owner
     );
+
+    event ItemListed(
+        uint id,
+        uint eCount,
+        uint dCount,
+        bool isListed
+    );
+
     
     function mintItem(uint256 _price, string memory _name, string memory _description, string memory _cid) public {
         require(_price > 0, "Invalid price");
@@ -42,21 +60,55 @@ contract Nfto is ERC721{
         emit ItemMinted(tokenID, _name, _description, msg.sender);
     }
 
-    function listItem(uint _id, string memory _type) public {
-        require(_id > 0 && _id <= tokenID , "Invalid Id");
+
+    function listItem(uint _id, string memory _type, uint256 _price) public {
+        require(_id > 0 && _id <= tokenID, "Invalid Id");
+
         if(keccak256(abi.encodePacked((_type))) == keccak256(abi.encodePacked(("english")))) {
             eCount++;
             english[eCount] = items[_id];
-
+            Item memory item = english[eCount];
+            item.latestPrice = _price;
+            english[eCount] = item;
         }
+
         else if(keccak256(abi.encodePacked((_type))) == keccak256(abi.encodePacked(("dutch")))) {
             dCount++;
             dutch[dCount] = items[_id];
+            Item memory item = dutch[dCount];
+            item.latestPrice = _price;
+            dutch[dCount] = item;
         }
+
         Item memory item = items[_id];
         item.isListed = true;
         items[_id] = item;
         approve(address(this),_id);
+        emit ItemListed(_id, eCount, dCount, item.isListed);
+    }
+
+
+    function makeOffer(uint _id, uint _tId, uint256 _amount, string memory _type) public {
+        require(_tId > 0 && _tId <= tokenID, "Invalid Id");
+        Item memory item = items[_tId];
+        require(item.isListed == true, "Item not listed");
+
+        if(keccak256(abi.encodePacked((_type))) == keccak256(abi.encodePacked(("english")))) {
+            require(_amount > item.latestPrice, "Invalid price");
+            Item memory item = english[_id];
+            item.latestPrice = _amount;
+            english[_id] = item;
+        }
+
+        else if(keccak256(abi.encodePacked((_type))) == keccak256(abi.encodePacked(("dutch")))) {
+            require(_amount < item.latestPrice, "Invalid price");
+            Item memory item = dutch[_id];
+            item.latestPrice = _amount;
+            dutch[_id] = item;
+        }
+
+        offerCount++;
+        offers[offerCount] = Offer(offerCount, _tId, _amount, msg.sender, false);
     }
 }
 
